474f7380ece167f13ea975dd65dbad4cb17ab887
raccoon-dev <raccoon@null.net>
2021-04-26 00:54:27 +0100

    Added style. Added icon.

--- LogServerWin/LogServerWin.dpr
+++ LogServerWin/LogServerWin.dpr
@@ -4,13 +4,17 @@ uses
   Vcl.Forms,
   uMain in 'uMain.pas' {frmMain},
   DNLog.Types in '..\DNetLog\DNLog.Types.pas',
-  DNLog.Server in '..\DNetLog\DNLog.Server.pas';
+  DNLog.Server in '..\DNetLog\DNLog.Server.pas',
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.Title := 'DNetLog Server';
+  TStyleManager.TrySetStyle('Windows10 SlateGray');
   Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
